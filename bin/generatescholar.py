c754ee715451ff34a74b474d063eb4d3f4775a5f
thomasot-git <67055029+thomasot-git@users.noreply.github.com>
2020-06-17 17:15:57 +0200

    Update generatescholar.py

--- bin/generatescholar.py
+++ bin/generatescholar.py
@@ -8,7 +8,7 @@ author = next(search_query).fill()
 # print(author)
 
 # Take a closer look at the first publication
-pub = author.publications[0].fill()
+pub = author.publications[4].fill()
 
 print(pub)
 
