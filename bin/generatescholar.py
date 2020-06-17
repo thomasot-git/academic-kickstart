6c066a67d1304f4e0b0fe27310c2610f685934da
thomasot-git <67055029+thomasot-git@users.noreply.github.com>
2020-06-17 16:43:11 +0200

    Update generatescholar.py

--- bin/generatescholar.py
+++ bin/generatescholar.py
@@ -5,7 +5,7 @@ from scholarly import scholarly
 search_query = scholarly.search_author('Carlo Fischione')
 author = next(search_query).fill()
 
-print(author)
+# print(author)
 
 # Take a closer look at the first publication
 pub = author.publications[0].fill()
