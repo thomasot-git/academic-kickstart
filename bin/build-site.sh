af359c44a406cac6558360981e67b0b247369d6c
thomasot-git <67055029+thomasot-git@users.noreply.github.com>
2020-06-17 15:59:13 +0200

    Update build-site.sh

--- bin/build-site.sh
+++ bin/build-site.sh
@@ -4,7 +4,7 @@ echo "Installing dependencies"
 pip3 install -U git+https://github.com/OrganicIrradiation/scholarly.git
 
 echo "Extracting Scholar data"
-# python bin/extractscholar.py OY4I82EAAAAJ
+python bin/generatescholar.py
 
 echo "Saving Scholar json on data folder"
 # mkdir -p data
