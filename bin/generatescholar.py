94caa84e763edc2c271d04b6f9dbb23d99a3d708
thomasot-git <67055029+thomasot-git@users.noreply.github.com>
2020-06-17 17:14:00 +0200

    Update generatescholar.py

--- bin/generatescholar.py
+++ bin/generatescholar.py
@@ -5,12 +5,12 @@ from scholarly import scholarly
 search_query = scholarly.search_author('Rong Du')
 author = next(search_query).fill()
 
-print(author)
+# print(author)
 
 # Take a closer look at the first publication
 pub = author.publications[0].fill()
 
-# print(pub)
+print(pub)
 
 file = open("publications.json","w")
 file.write(json.dumps(pub))
